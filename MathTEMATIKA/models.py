eb8ee48a3a4f2a6aef66035b31873d73846dd192
Драган Ћајић <cajic_dragan@yahoo.com>
2024-02-09 00:22:35 +0100

    ~ defini$i modele & napravi/pokreni `migracije` [formiraj bazu] :minidisc: :running_man:
    ``` bash
    dragancajic@localhost:~/PycharmProjects/mathtematika> source .venv/bin/activate
    
    (.venv) dragancajic@localhost:~/PycharmProjects/mathtematika> python3.10 manage.py makemigrations MathTEMATIKA
    Migrations for 'MathTEMATIKA':
      MathTEMATIKA/migrations/0001_initial.py
        - Create model Category
        - Create model Post
        - Create model Comment
    
    (.venv) dragancajic@localhost:~/PycharmProjects/mathtematika> python3.10 manage.py migrate MathTEMATIKA
    Operations to perform:
      Apply all migrations: MathTEMATIKA
    Running migrations:
      Applying MathTEMATIKA.0001_initial... OK
    
    '''**још увијек све ради!**'''
    (.venv) dragancajic@localhost:~/PycharmProjects/mathtematika> python3.10 manage.py runserver
    Watching for file changes with StatReloader
    Performing system checks...
    
    System check identified no issues (0 silenced).
    
    February 08, 2024 - 23:22:03
    Django version 4.2.2, using settings 'mathtematika.settings'
    Starting development server at http://127.0.0.1:8000/
    Quit the server with CONTROL-C.
    ```

--- MathTEMATIKA/models.py
+++ MathTEMATIKA/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+
+    # нови пост направљен -> сачувај датум
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    # пост измијењен/едитован -> сачувај датум
+    last_modified = models.DateTimeField(auto_now=True)
+
+    # у једној категорији може бити више постова, али исто тако
+    # један пост може припадати различитим категоријама, ДАКЛЕ
+    # веза је више-на-према-више -> many-to-many, m:n
+    categories = models.ManyToManyField("Category", related_name="posts")
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    # страни кључ у табели за коментаре, јер један пост може имати више коментара,
+    # ДАКЛЕ, веза је један-на-према-више -> 1:n, а на страни `више` иде страни кључ;
+    # приликом брисања поста, бришу се и коментари везани за тај пост - има логике!
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
