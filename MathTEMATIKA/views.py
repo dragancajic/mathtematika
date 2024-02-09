e4097ec452cb009e9afab88c0563ac4fca6f34cb
Драган Ћајић <cajic_dragan@yahoo.com>
2024-02-09 04:59:33 +0100

    ~ napisi logiku za front-end @ `views`
    • функције за:
    
    - приказ свих постова, од најновијег ка старијим,
    - приказ свих постова за тражену категорију,
    - детаљни приказ поста са коментарима, ако их има.

--- MathTEMATIKA/views.py
+++ MathTEMATIKA/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from MathTEMATIKA.models import Post, Comment
 
 # Create your views here.
+
+# излистај све постове
+def blog_index(request):
+    # `-` уреди у опадајућем поретку, од најновијег поста!
+    posts = Post.objects.all().order_by("-created_on")
+
+    context = {
+        "posts": posts,
+    }
+    return render(request, "MathTEMATIKA/index.html", context)
+
+
+# излистај све постове за одређену категорију
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__names__contains = category
+    ).order_by("-created_on")
+
+    context = {
+        "category": category,
+        "posts": posts,
+    }
+    return render(request, "MathTEMATIKA/category.html", context)
+
+
+# прикажи читав садржај поста -- све детаље
+def blog_detail(request, pk):
+    post = Post.objects.get(pk = pk)
+    comments = Comment.objects.filter(post = post)
+
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+    return render(request, "MathTEMATICA/detail.html", context)
