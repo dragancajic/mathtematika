c96186850be25cef04b08beffac0df88640d2e9c
Драган Ћајић <cajic_dragan@yahoo.com>
2024-02-09 03:44:46 +0100

    ~ poPUNI bazu podacima koristeci `Django Admin Site` :superhero: :ok_hand:
    You have 18 unapplied migration(s).
    Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
    Run 'python manage.py migrate' to apply them.
    
    ``` bash
    '''покрени default Django миграције, прије креирања supreuser-а!'''
    (.venv) dragancajic@localhost:~/PycharmProjects/mathtematika> python3.10 manage.py migrate
    Operations to perform:
      Apply all migrations: MathTEMATIKA, admin, auth, contenttypes, sessions
    
    '''направи superuser-а да би користио погодности `Django Admin Site` веб интерфејса'''
    (.venv) dragancajic@localhost:~/PycharmProjects/mathtematika> python3.10 manage.py createsuperuser
    Username (leave blank to use 'dragancajic'):
    Email address: cajic_dragan@yahoo.com
    Password:
    Password (again):
    Superuser created successfully.
    
    (.venv) dragancajic@localhost:~/PycharmProjects/mathtematika> python3.10 manage.py runserver
    ```
    
    Site administration | Django site admin
    http://localhost:8000/admin

--- MathTEMATIKA/admin.py
+++ MathTEMATIKA/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from MathTEMATIKA.models import Category, Post, Comment
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
