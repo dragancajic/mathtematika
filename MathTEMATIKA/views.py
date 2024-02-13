36dc2f8b519b596409cb578526872f7fefd9906b
Драган Ћајић <cajic_dragan@yahoo.com>
2024-02-13 23:58:42 +0100

    ~ dodaj `Django Form` | `Komentari na objave` :speaking_head: :speaker: :family:
    PyCharm 2023.3.3 (Professional Edition) | 2.9 GiB -> 7.7 GiB / 11.1 GiB
    PyCharm 2023.3.3 (Community Edition)    | 1.6 GiB -> 6.4 GiB / 11.1 GiB √
    PyCharm 2022.2.2 (Edu)                  | 2.5 GiB -> 7.4 GiB / 11.1 GiB
    
    TODO: форматирај датуме у складу са локалним поставкама `dan.mjesec.godina.` -> 13.02.2024.
    
    TODOs:
    
    » Rendering form error messages -> Working with forms | Django documentation | Django
    https://docs.djangoproject.com/en/4.2/topics/forms/#rendering-form-error-messages
    
    » Accessing “clean” data -> The Forms API | Django documentation | Django
    https://docs.djangoproject.com/en/4.2/ref/forms/api/#accessing-clean-data

--- MathTEMATIKA/views.py
+++ MathTEMATIKA/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from MathTEMATIKA.forms import CommentForm
 from MathTEMATIKA.models import Post, Comment
+
 
 # Create your views here.
 
@@ -17,7 +21,7 @@ def blog_index(request):
 # излистај све постове за одређену категорију
 def blog_category(request, category):
     posts = Post.objects.filter(
-        categories__name__contains = category
+        categories__name__contains=category
     ).order_by("-created_on")
 
     context = {
@@ -29,11 +33,26 @@ def blog_category(request, category):
 
 # прикажи читав садржај поста -- све детаље
 def blog_detail(request, pk):
-    post = Post.objects.get(pk = pk)
-    comments = Comment.objects.filter(post = post)
+    post = Post.objects.get(pk=pk)
+    form = CommentForm()  # направи "празну" форму/образац за попуњавање :-)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+
+    comments = Comment.objects.filter(post=post)
 
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
     return render(request, "MathTEMATIKA/detail.html", context)
