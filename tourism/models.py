bee622a03fa4b25fbbeb256bb54b691b5c97d945
Драган Ћајић <cajic_dragan@yahoo.com>
2024-06-19 21:31:58 +0200

    ~ napisi modele po UML dijagramu | `create Model classes`
    'Туристичке <a href="https://turizamrs.org/destinacije/">дестинације</a> Републике Српске'
    
    ``` bash
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ python manage.py makemigrations
    Migrations for 'tourism':
      tourism/migrations/0001_initial.py
        - Create model Location
        - Create model User
        - Create model Preference
        - Create constraint location_name_case_insensitive_unique on model location
    ...
    FileNotFoundError: [Errno 2] No such file or directory: '/home/dragancajic/.local/bin/black'
    ```
    
    ~ import-error / E0401 - Pylint 3.3.0-dev0 documentation
    https://pylint.readthedocs.io/en/latest/user_guide/messages/error/import-error.html
    
    ▪ Overview | Improve Your Code with Pylint and Black | Adafruit Learning System
    https://learn.adafruit.com/improve-your-code-with-pylint/overview
    
    Check your code with pre-commit | Creating and sharing a CircuitPython library | Adafruit Learning System <-- LOOK!
    https://learn.adafruit.com/creating-and-sharing-a-circuitpython-library/check-your-code
    
    Adafruit_Learning_System_Guides/.pylintrc at main · adafruit/Adafruit_Learning_System_Guides
    https://github.com/adafruit/Adafruit_Learning_System_Guides/blob/main/.pylintrc
    
    ``` bash
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ pip list | grep 'black'
    
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ pip install black
    Installing collected packages: mypy-extensions, black
    Successfully installed black-24.4.2 mypy-extensions-1.0.0
    
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ pylint --version
    pylint 3.2.3
    astroid 3.2.2
    Python 3.11.6 (main, Oct  8 2023, 05:06:43) [GCC 13.2.0]
    
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ pip list | grep 'black'
    black                     24.4.2
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ _
    
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ python manage.py makemigrations
    No changes detected
    
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ python manage.py migrate
    Operations to perform:
      Apply all migrations: admin, auth, contenttypes, events, sessions, tourism
    Running migrations:
      Applying tourism.0001_initial... OK
      Applying tourism.0002_alter_location_name... OK
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ _
    ```
    
    ~ A practical guide to using the git stash command | Opensource.com
    https://opensource.com/article/21/4/git-stash
    
    ``` bash
    (.venv) dragancajic@ubuntu:~/PycharmProjects/mathtematika$ git stash list
    stash@{0}: On 1--struktura-html: sacuvaj stanje prije kodiranja modela
    ```

--- tourism/models.py
+++ tourism/models.py
@@ -1,3 +1,76 @@
+"""
+~ МОДЕЛ ПОДАТАКА / `Models` as Python classes in Django
+"""
+
 from django.db import models
+from django.db.models import (
+    UniqueConstraint,
+)  # ограничи поља на јединствене вриједности/без дуплих уноса
+from django.db.models.functions import Lower  # за мала слова вриједности поља
+
 
 # Create your models here.
+class Location(models.Model):
+    """
+    Модел који представља туристичко мјесто/дестинацију/локацију.
+    """
+
+    name = models.CharField(
+        max_length=20,
+        unique=True,  # име туристичке дестинације је јединствено (case-sensitive!)
+        help_text='Туристичке \
+<a href="https://turizamrs.org/destinacije/">дестинације</a> Републике Српске',
+    )
+    po_box = models.CharField(max_length=5)
+
+    def __str__(self):
+        """Стринг који представља на читљив људима начин објекат модела."""
+        return self.name
+
+    class Meta:
+        """Meta класа за додатне информације o моделу"""
+
+        constraints = [
+            UniqueConstraint(
+                Lower("name"),
+                name="location_name_case_insensitive_unique",
+                violation_error_message="Локација већ постоји (велика/мала слова нису битна!)",
+            ),
+        ]
+
+
+class Preference(models.Model):
+    """
+    Модел који представља жеље/свиђања/преференце корисника, тј. будућег туристе.
+    """
+
+    # приликом брисања корисника - обриши и његове преференце
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="preferences"
+    )
+    food = models.CharField(max_length=30, null=True)
+    drink = models.CharField(max_length=30, null=True)
+    entertainment = models.CharField(max_length=30, null=True)
+    recreation = models.CharField(max_length=30, null=True)
+    sport = models.CharField(max_length=30, null=True)
+    hobby = models.CharField(max_length=50, null=True)
+    comment = models.TextField(max_length=1000, default="Још увијек нема коментара")
+
+    def __str__(self):
+        return f"{self.user.name}'s preferences"
+
+
+class User(models.Model):
+    """
+    Модел који представља корисника, тј. будућег или бившег туристу.
+    """
+
+    name = models.CharField(
+        max_length=50, help_text="Ваше име или надимак, по жељи! :-)"
+    )
+    email = models.EmailField(null=True)
+    region = models.CharField(max_length=20)
+    state = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"{self.name} from {self.region}"
